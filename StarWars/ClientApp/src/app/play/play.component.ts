a13b4cf99bd489ce112a00dbe6bca25d5c587afb
Michał Mamcarz <michal.mamcarz@deva.as>
2019-10-29 14:12:07 +0100

    score counter

--- StarWars/ClientApp/src/app/play/play.component.ts
+++ StarWars/ClientApp/src/app/play/play.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin  } from 'rxjs';
 import { PeopleService } from '../services/people/people.service';
 import { StarsShipService } from '../services/starsShip/starsShip.service';
+import { getMatScrollStrategyAlreadyAttachedError } from '@angular/cdk/overlay/typings/scroll/scroll-strategy';
 
 @Component({
   selector: 'app-play',
@@ -16,7 +17,10 @@ export class PlayComponent implements OnInit {
   private type: string;
   public my$: Observable<any>;
   public random$: Observable<any>;
-    constructor(private route: ActivatedRoute, private ps: PeopleService, private st: StarsShipService) { }
+  public score: number;
+    constructor(private route: ActivatedRoute, private ps: PeopleService, private st: StarsShipService) { 
+      this.score = 0;
+    }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
@@ -30,9 +34,32 @@ export class PlayComponent implements OnInit {
       this.my$ = this.st.get(this.id);
       this.random$ = this.st.random();
     }
+
+    this.sub();
   }
 
+  private sub() {
+    forkJoin([this.my$, this.random$]).subscribe(results => {
+      if (this.type === 'people') {
+        const myMass = results[0].mass;
+        const randomMass = results[1].mass;
+        this.getScore(myMass, randomMass);
+      }
+      if (this.type === 'starsShip') {
+        const myMass = results[0].cargoCapacity;
+        const randomMass = results[1].cargoCapacity;
+        this.getScore(myMass, randomMass);
+      }
+    });
+  }
 
+  getScore(my: number, random: number) {
+    if (my > random) {
+      this.score++;
+    } else if (my < random) {
+      this.score--;
+    }
+  }
 
   playAgain() {
     if (this.type === 'people') {
@@ -41,6 +68,8 @@ export class PlayComponent implements OnInit {
     if (this.type === 'starsShip') {
       this.random$ = this.st.random();
     }
+
+    this.sub();
   }
 
 }
